7d55b1b17363439575a745f12fbe1c40e0a7f321
Isaiah Carrin <104584679+iCarrin@users.noreply.github.com>
2025-10-28 19:07:09 -0600

    more page running, and deleting old documents that don't belong to this repo

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function App(){
   const experienceList = [  {title:"BYU-I", timePeriod:"2026", location:"Rexburg, ID", description:"Getting my bachelor's degree in computer science with an emphasis in machine learning."},
                             {title:"Other Jobs", timePeriod:"2013 - 2025", location:"Georgia Idaho Utah", description:"I don't have any industry related work yet to put up here, but I've had a job since I was 15. I've worked all over in just about everything, so some skills are bound to be important. (customer relations, sales, creative problem solving, etc)"},
                           ];
-  const paragraphList = ["I grew up in Georgia but after serving a proselyting mission for the Church of Jesus Christ of Latter-Day Saint I moved out to Idaho for college, and that's still where I currently live.",
-                          "I have the ability to add another paragraph here dynamically, but I can't think of anything to put."];
+  const paragraphList = [ "Hey, I'm Isaiah. I'm a computer science student at BYU-Idaho with a passion for learning and creating. I enjoy web development, QA, and general problem solving. That's why I got into programming because I love solving puzzles.", 
+                          "When I'm not coding (or slaving over homework) I enjoy being with my family, 3D printing, making a cosplay or halloween costumes, and working on house projects and improvements.", 
+                          "I'm almost done with school and looking for what I want to do with my degree and where I'll find my internship."];
 
     
   return(
@@ -26,7 +27,29 @@ function App(){
       <div id="Experience">
         <ExperienceList  experiences={experienceList}/>
       </div>
-      <div id="Projects">
+      <div id="Projects">          
+
+        <Card 
+              cartoonImage="./src/assets/dna.png" 
+              // Freepik on flaticon.com https://www.flaticon.com/authors/freepik
+              realImage="\src\assets\DNA real picture.jpg" 
+              projectName="DNA Primer Multiplexer" 
+              link="https://github.com/EldonBird/Bio_dpt"
+              description="Finding SNP specific primers that can all replicate without interfering with one another. I've worked as a devops, a project manager finally the project lead."/>
+        <Card 
+              cartoonImage="./src/assets/recipe-book.png" 
+              // Freepik on flaticon.com https://www.flaticon.com/authors/freepik
+              realImage="\src\assets\scuttle real.jpg" 
+              projectName="Scuttle" 
+              link="https://github.com/celeste-wahlquist/2025_I-Hack"
+              description="Hackathon project that scrapped recipes from the web to be used to generate meal plans and shopping lists from those meal plans. Unfinished"/>
+        <Card 
+              cartoonImage="./src/assets/checklist.png" 
+              // Shashank Singh on flaticon.com  https://www.flaticon.com/authors/Shashank
+              realImage="\src\assets\Habitlings.jpg" 
+              projectName="Habitlings" 
+              link="https://github.com/iCarrin/Habitlings"
+              description="A habit tracker that uses tamagotchi like pets who die if you don't keep up with them. Only image and check boxes ever implemented."/> 
         <Card 
               cartoonImage="./src/assets/garden-icon-design-free-vector.jpg" 
               realImage="./src/assets/Garden Planner real.jpg" 
@@ -39,18 +62,6 @@ function App(){
               projectName="Portfolio Web Page" 
               link="#Home"
               description="This is the site you're looking at. I created this as a way to delve into React. I got a lot of practice making this. I used just the classing HTML, CSS, JavaScript, and some built in React functions"/>
-
-                    {/* scuttle
-      DNA
-      Habitlings
-      google tasks api */}
-        <Card 
-              cartoonImage="./src/assets/dna.png" 
-              // Freepik on flaticon.com https://www.flaticon.com/authors/freepik
-              realImage="\src\assets\DNA real picture.jpg" 
-              projectName="DNA Primer Multiplexer" 
-              link="https://github.com/EldonBird/Bio_dpt"
-              description="Finding SNP specific primers that can all replicate without interfering with one another. I've worked as a devops, a project manager finally the project lead."/>
         <Card 
               cartoonImage="./src/assets/code.png" 
               //Shahid-Mehmood on flaticon.com  https://www.flaticon.com/authors/Shahid-Mehmood
@@ -58,20 +69,7 @@ function App(){
               projectName="Google tasks API experiment" 
               link="https://github.com/iCarrin/google-tasks-api-practice"
               description="Drew up a quick Google tasks editor to practice working with API's, and google OAuth"/>
-        <Card 
-              cartoonImage="./src/assets/checklist.png" 
-              // Shashank Singh on flaticon.com  https://www.flaticon.com/authors/Shashank
-              realImage="\src\assets\Habitlings.jpg" 
-              projectName="Habitlings" 
-              link="https://github.com/iCarrin/Habitlings"
-              description="A habit tracker that uses tamagotchi like pets who die if you don't keep up with them. Only image and check boxes ever implemented."/>           
-        <Card 
-              cartoonImage="./src/assets/recipe-book.png" 
-              // Freepik on flaticon.com https://www.flaticon.com/authors/freepik
-              realImage="\src\assets\scuttle real.jpg" 
-              projectName="Scuttle" 
-              link="https://github.com/celeste-wahlquist/2025_I-Hack"
-              description="Hackathon project that scrapped recipes from the web to be used to generate meal plans and shopping lists from those meal plans"/>      
+
       </div>
       <Footer/>
       {/* hero image - I want an interactive background
